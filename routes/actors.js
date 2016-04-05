3d1be0af8b6af3ba384ab72c062de293376b0e62
Antoine Gagné <gagnantoine@gmail.com>
2016-04-05 11:01:17 -0400

    Adds CORS OPTIONS to actors route

--- routes/actors.js
+++ routes/actors.js
@@ -1,13 +1,22 @@
 'use strict';
 
+var cors = require('cors');
 var express = require('express');
 var router  = express.Router();
+
 var routes  = require('./methods/actors');
 
+var corsOptions = {
+    origin: '*',
+    methods: ['GET']
+};
+
 router.route('/search/actors')
+    .options(cors(corsOptions))
     .get(routes.findActors);
 
 router.route('/actors')
+    .options(cors(corsOptions))
     .get(routes.getActor);
 
 exports.router = router;
