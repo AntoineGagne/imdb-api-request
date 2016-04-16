c642ea6c8a6f5de7e047e91aa7907f4871eae425
Antoine Gagné <gagnantoine@gmail.com>
2016-04-16 00:04:46 -0400

    Revert "Adds https handling"
    This reverts commit d31125dd0c466e4959d9ddb539e9836ec67c0aee.

--- server.js
+++ server.js
@@ -3,19 +3,12 @@
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var express = require('express');
-var fileSystem = require('fs');
-var https = require('https');
 
 var medias = require('./routes/medias');
 var actors = require('./routes/actors');
 
 var port = process.env.PORT || 5000;
 var app  = express();
-
-var options = {
-    key : fileSystem.readFileSync('server.key'),
-    cert: fileSystem.readFileSync('server.crt')
-};
 
 var corsOptions = {
     origin: true,
@@ -27,8 +20,9 @@ var corsOptions = {
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
+app.options('*', cors(corsOptions));
 app.use('/', medias.router);
 app.use('/', actors.router);
 
-https.createServer(options, app).listen(port);
+app.listen(port);
 console.log('Listening on port ' + port);
