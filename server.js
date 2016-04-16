d31125dd0c466e4959d9ddb539e9836ec67c0aee
Antoine Gagné <gagnantoine@gmail.com>
2016-04-15 23:43:35 -0400

    Adds https handling

--- server.js
+++ server.js
@@ -3,12 +3,19 @@
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var express = require('express');
+var fileSystem = require('fs');
+var https = require('https');
 
 var medias = require('./routes/medias');
 var actors = require('./routes/actors');
 
 var port = process.env.PORT || 5000;
 var app  = express();
+
+var options = {
+    key : fileSystem.readFileSync('server.key'),
+    cert: fileSystem.readFileSync('server.crt')
+};
 
 var corsOptions = {
     origin: true,
@@ -20,9 +27,8 @@ var corsOptions = {
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
-app.options('*', cors(corsOptions));
 app.use('/', medias.router);
 app.use('/', actors.router);
 
-app.listen(port);
+https.createServer(options, app).listen(port);
 console.log('Listening on port ' + port);
