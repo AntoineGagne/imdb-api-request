a571f4162693a14c9443ee5518f22b2d07219b1d
Antoine Gagné <gagnantoine@gmail.com>
2016-04-16 00:29:10 -0400

    Tests setting the credentials

--- server.js
+++ server.js
@@ -11,15 +11,14 @@ var port = process.env.PORT || 5000;
 var app  = express();
 
 var corsOptions = {
-    origin: true,
-    allowedHeaders: ['Content-Type', 'Accept', 'X-Requested-With', 'Origin'],
+    origin: '*',
     methods: ['POST', 'GET'],
+    credentials: true
 };
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
-app.options('*', cors(corsOptions));
 app.use('/', medias.router);
 app.use('/', actors.router);
 
